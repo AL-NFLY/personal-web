85c4854a61ac3cd3e2b6727348bb19853e8cec7f
AL-NFLY <almernfly@gmail.com>
2023-09-11 16:14:54 +0700

    feat: Projects schema added in admin page

--- frontend-next/sanity.config.ts
+++ frontend-next/sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import project from "./sanity/schemas/project-schema";
 
 const config = defineConfig({
     projectId: "giahi441",
@@ -7,7 +8,8 @@ const config = defineConfig({
     title: "Personal Website",
     apiVersion: "2023-11-09",
     basePath: "/admin",
-    plugins: [deskTool],
+    plugins: [deskTool()],
+    schema: { types: [project] }
 })
 
 export default config
