38459b4647d08e052dbd21162813630717b5bb69
AL-NFLY <almernfly@gmail.com>
2023-09-12 18:42:11 +0700

    feat: Polish Project section

--- frontend-next/src/app/page.tsx
+++ frontend-next/src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { getProjects } from "../../sanity/sanity-utils";
+import Link from "next/link";
+import { PortableText } from "@portabletext/react";
 
 export default async function Home() {
   const projects = await getProjects();
@@ -21,25 +23,30 @@ export default async function Home() {
           <span className="font-bold text-gray-700 text-2xl md:text-3xl bg-gradient-to-r from-cyan-300 to-red-300 dark:bg-clip-text dark:text-transparent">My Projects</span>
           <p className="mt-2">Here is a list of my projects, feel free to check them out!</p>
 
-          <div className="mt-6 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+          <div className="mt-6 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 ">
             {projects.map((project)=>(
-              <div 
+              <Link 
+                href={`/projects/${project.slug}`}
                 key={project._id}
-                className="border border-gray-500 rounded-md"
+                className="border-2 border-gray-600 hover:border-black rounded-sm hover:scale-[1.05] active:scale-[0.95] transition duration-300"
               >
                 {project.image && (
                   <Image
                   src={project.image}
                   alt={project.name}
-                  width={250}
-                  height={100}
-                    className="object-cover rounded-md border border-gray-500"
+                  width={500}
+                  height={300}
+                    className="object-cover rounded-t-sm border border-gray-500"
                     />
                     )}
-                <span className="font-extrabold bg-gradient-to-r from-cyan-300 to-red-300 dark:bg-clip-text dark:text-transparent">
-                {project.name}
-                  </span>
-              </div>
+                  <div className="px-6 py-3 leading-6">
+                    <span className="font-extrabold bg-gradient-to-r from-cyan-300 to-red-300 dark:bg-clip-text dark:text-transparent">
+                      {project.name}
+                    </span>
+                    <div className="mt-1 text-sm  "><PortableText value={project.content} /></div>
+                    <p className="text-sm mt-4"> Read More →</p>
+                  </div>  
+              </Link>
               ))}
           </div>
         </article>
