53ca962bcfaafe14a215f705ae5de077d8e8cbc4
AL-NFLY <almernfly@gmail.com>
2023-09-13 22:59:38 +0700

    feat: Change Project page layout

--- frontend-next/src/app/projects/[project]/page.tsx
+++ frontend-next/src/app/projects/[project]/page.tsx
@@ -1,4 +1,6 @@
 import { getProject } from "../../../../sanity/sanity-utils"
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
 
 type Props = {
     params: { project: string }
@@ -9,6 +11,50 @@ export default async function Project({ params }: Props){
     const project = await getProject(slug)
 
     return(
-        <div>{project.name}</div>
+        <main className="bg-dark h-screen">
+            <section className="max-w-3xl mx-auto py-20">
+                <div>
+                    <Image 
+                        src={project.image}
+                        alt={project.name}
+                        width={1920}
+                        height={1080}
+                        className="rounded-md object-contain"
+                    />
+                    <div className="flex flex-col items-start mt-6">
+                        <h1 className="text-3xl font-bold bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">{project.name}</h1>
+                        <ul className="mt-1 list-inside list-disc px-3 text-sm text-zinc-300">
+                            <li>
+                                <a 
+                                    href={project.url}
+                                    title="View Project"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="hover:font-bold hover:text-white duration-150"
+                                    >
+                                        Live Link
+                                </a>  
+                            </li>
+                            <li>
+                                <a 
+                                    href='https://github.com/AL-NFLY/'
+                                    title="View Project"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="hover:font-bold hover:text-white duration-150"
+                                    >
+                                        Github Link
+                                </a>    
+                            </li>
+                        </ul>
+                        <div className="mt-3 border-t pt-3">
+                            <PortableText value={project.content} />
+                        </div>
+                    </div>
+
+                </div>
+
+            </section>
+        </main>
     )
 }
