d0001fc6f9151c39d4a165d57bde78e20e412d00
AL-NFLY <almernfly@gmail.com>
2023-09-15 15:51:05 +0700

    feat: Add social media icons for later usage

---
+++ frontend-next/src/app/assets/icons/index.ts
@@ -0,0 +1,16 @@
+import github from '../icons/github.svg';
+import gmail from "../icons/gmail.svg";
+import linkedIn from "../icons/linkedin.svg";
+import twitter from "../icons/twitter.svg";
+import instagram from "../icons/instagram.svg";
+import hamburger from "../icons/hamburger.svg";
+
+export {
+    github,
+    gmail,
+    linkedIn,
+    twitter,
+    instagram,
+    
+    hamburger,
+}
