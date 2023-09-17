b756219525892074f9b47695c74cb602a49e9319
AL-NFLY <almernfly@gmail.com>
2023-09-18 00:32:00 +0700

    feat: Add footer

--- frontend-next/next.config.js
+++ frontend-next/next.config.js
@@ -8,6 +8,9 @@ const nextConfig = {
             port: '',
           },
         ],
+        dangerouslyAllowSVG: true,
+        contentDispositionType: 'attachment',
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
       },
 }
 
--- frontend-next/src/components/footer.tsx
+++ frontend-next/src/components/footer.tsx
@@ -1,8 +1,58 @@
+// import { github, gmail, linkedIn } from '@/app/assets/icons';
+import {  IoLogoGithub, IoLogoLinkedin, IoMail } from "react-icons/io5";
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
+import { IconType } from "react-icons";
+
+type Social = {
+  href: string;
+  icon: IconType;
+  id: string;
+  alt: string;
+}
+
+const socials: Social[] = [
+  {
+    href: 'https://github.com/AL-NFLY',
+    icon: IoLogoGithub,
+    id: 'Github',
+    alt: 'github logo',
+  },
+  {
+    href: 'https://www.linkedin.com/in/almer-naufaly/',
+    icon: IoLogoLinkedin,
+    id: 'LinkedIn',
+    alt: 'linkedIn logo',
+  },
+  {
+    href: 'mailto://almer.nfly@gmail.com',
+    icon: IoMail,
+    id: 'Email',
+    alt: 'email logo',
+  },
+];
+
 
 const Footer = () => {
   return (
-    <footer></footer>
+  
+    <footer className='xl:px-48 max-sm:px-8'>
+      <main className='mt-8 flex flex-col flex-wrap items-center border-t border-zinc-500 text-zinc-300'>
+        <p className="mt-8">Hit me up!</p>
+        <div className='flex space-x-4'>
+          {socials.map((social) => (
+            <>
+              <Link href={social.href} target='_blank' className=""
+              >
+                <social.icon className="mt-1 h-10 w-10 p-1 rounded-sm hover:bg-gradient-to-r from-primary to-secondary hover:text-dark duration-150 active:text-dark/80"/>
+              </Link>
+            </>
+          ))}
+          </div>
+        <p className='mt-4 text-sm text-zinc-300'>© 2023 Almer Naufaly</p>
+      </main>
+    </footer> 
     )
 }
 
