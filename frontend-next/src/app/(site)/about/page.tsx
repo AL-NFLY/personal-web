e9e815205f90e72c076186c73fd4e23728aaf5f1
AL-NFLY <almernfly@gmail.com>
2023-09-29 12:08:10 +0700

    feat: Adjust /about layout for mobile layout

--- frontend-next/src/app/(site)/about/page.tsx
+++ frontend-next/src/app/(site)/about/page.tsx
@@ -22,9 +22,11 @@ export default async function About() {
 
                     <>
                         {profile.map((me) => (
-                            <article className="flex space-x-8 mt-6" key={me._id}>
+                            <article className="flex max-sm:flex-col space-x-8 mt-6" key={me._id}>
+                                <figure className="flex items-center justify-center">
                                     <Image src={me.image} alt={me.name} width={175} height={300} className="rounded-sm"></Image>
-                                <div className="max-w-xl leading-relaxed text-zinc-600 dark:text-zinc-300">
+                                </figure>
+                                <div className="max-w-xl leading-relaxed text-zinc-600 dark:text-zinc-300 max-sm:mt-6">
                                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque aperiam, accusantium ad suscipit repellendus laborum temporibus adipisci itaque dolorum repellat cupiditate iusto id error laboriosam ratione inventore vero porro reiciendis!</p>
                                     <p className="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque libero dolorem alias fuga odit facere nihil laborum reprehenderit quisquam molestias numquam atque perferendis minima eum at recusandae, ipsam error? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque quod quas sit quasi dolorum fuga hic sapiente veniam, ullam nostrum, facilis repellat aspernatur? Ex veniam eligendi itaque sunt deserunt nemo?</p>
                                 </div>
