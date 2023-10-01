4086387136e112be3d80075bf9e0cd87c1bf25fa
AL-NFLY <almernfly@gmail.com>
2023-10-01 21:44:12 +0700

    feat: Adjust ProjectCards flexibility

--- frontend-next/src/components/ProjectCards.tsx
+++ frontend-next/src/components/ProjectCards.tsx
@@ -15,7 +15,7 @@ const ProjectCards = async() => {
             </h2>
             <p className="mt-2 dark:text-zinc-300 text-zinc-600">Here is a list of my projects, feel free to check them out!</p>
 
-            <ul className="mt-6 grid md:grid-cols-2 lg:grid-cols-3 gap-6">{projects.map((project) => (
+            <ul className="mt-6 grid md:grid-cols-2 xl:grid-cols-3 gap-6">{projects.map((project) => (
                 <li 
                 className="group rounded-lg border border-b-4 border-zinc-300 dark:border-zinc-500 hover:border-b-primary dark:hover:border-b-primary hover:-translate-y-2 active:scale-[0.98] duration-300 transition-all"
                 key={project._id}
@@ -32,13 +32,13 @@ const ProjectCards = async() => {
                     )}
                     
                     <div className="px-8 py-4 max-sm:px-4 max-sm:py-3">
-                        <span className="font-extrabold dark:text-primary group-hover:bg-gradient-to-r  from-primary to-secondary group-hover:dark:bg-clip-text group-hover:dark:text-transparent group-hover:bg-red-300 duration-50">
+                        <span className="font-extrabold text-sm lg:text-base dark:text-primary group-hover:bg-gradient-to-r from-primary to-secondary group-hover:dark:bg-clip-text group-hover:dark:text-transparent group-hover:bg-red-300 duration-50">
                         {project.name}
                         </span>
-                        <div className="mt-1 text-sm text-zinc-600 dark:text-zinc-300 tracking-wide">
+                        <div className="mt-1 text-xs lg:text-sm text-zinc-600 dark:text-zinc-300 tracking-wide">
                             <p>{project.overview}</p>
                         </div>
-                        <p className="text-sm pt-4 max-sm:pt-3"> Read More →</p>
+                        <p className="text-xs lg:text-sm pt-4 max-sm:pt-3"> Read More →</p>
                     </div>  
                 </Link>
                 </li>
