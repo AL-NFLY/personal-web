f67498113b097737fc4837a168c8be8cbff717ab
AL-NFLY <almernfly@gmail.com>
2023-10-02 16:01:51 +0700

    refactor: Remove <main> in global layout.tsx

--- frontend-next/src/app/(site)/layout.tsx
+++ frontend-next/src/app/(site)/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     {/* <body className={inter.className}> */}
       <Providers>
         <Header />
-        <main>{children}</main>
+        {children}
         <Footer />
       </Providers>
     </body>
