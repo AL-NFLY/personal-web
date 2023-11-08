52dbd3f91a3c4fb626396b0ba23c65e1771c6040
AL-NFLY <almernfly@gmail.com>
2023-11-08 18:02:50 +0700

    feat: Adjust cards animation in ProjectCards.tsx for lightmode

--- frontend-next/src/components/ProjectCards.tsx
+++ frontend-next/src/components/ProjectCards.tsx
@@ -17,7 +17,7 @@ const ProjectCards = async() => {
 
             <ul className="mt-6 grid md:grid-cols-2 xl:grid-cols-3 gap-y-6 gap-x-4 lg:gap-x-6">{projects.map((project) => (
                 <li 
-                    className="flex justify-center items-center group px-2 pt-2 rounded-lg bg-gradient-to-r from-zinc-300 to-zinc-300 dark:from-zinc-500 dark:to-zinc-500 dark:hover:from-primary dark:hover:to-secondary bg-bottom bg-no-repeat bg-[length:100%_4px] bg-zinc-800/50 hover:bg-zinc-800 hover:-translate-y-2 active:-translate-y-0.5 duration-300"
+                    className="flex justify-center items-center group px-2 pt-2 rounded-lg bg-gradient-to-r from-zinc-300 to-zinc-300 dark:from-zinc-500 dark:to-zinc-500 hover:from-primary hover:to-secondary dark:hover:from-primary dark:hover:to-secondary bg-bottom bg-no-repeat bg-[length:100%_4px] bg-zinc-800/50 hover:bg-zinc-800 hover:-translate-y-2 active:-translate-y-0.5 duration-300"
                     // className="flex justify-center items-center group rounded-lg border border-b-4 border-zinc-300 dark:border-zinc-500 hover:border-b-primary dark:hover:border-b-primary hover:-translate-y-2 active:scale-[0.98] duration-300 transition-all"
                     key={project._id}
                 >
