21b0051e3b7fd4087ec458188920dc136be7a224
AL-NFLY <almernfly@gmail.com>
2023-11-09 15:14:44 +0700

    feat: Change style of buttons in hero section

--- frontend-next/src/app/(site)/page.tsx
+++ frontend-next/src/app/(site)/page.tsx
@@ -19,21 +19,25 @@ export default async function Home() {
           </h1>
           <p className="mt-4 text-dark dark:text-zinc-300 tracking-wide">A Software Engineer & Bachelor of Engineering in Computer Eng. ITS</p>
 
-          <div className="flex mt-6 gap-4">
-            <Link
+          <div className="flex mt-6 gap-x-6">
+          <Link
               href='https://drive.google.com/file/d/1SVcxmopbjgWPImLW-GdryXJAZv1jGiOy/view?usp=sharing'
               target="_blank"
-              className="rounded-sm px-4 py-1 border dark:border-none hover:scale-105 bg-gradient-to-r from-primary to-secondary text-dark font-bold shadow-lg hover:shadow-secondary active:scale-100 duration-300">
+              className="rounded-md px-6 py-2 ring-1 ring-inset ring-secondary/10 bg-zinc-800/50 bg-gradient-to-r from-zinc-500 to-zinc-500 hover:from-secondary hover:to-secondary bg-bottom bg-no-repeat bg-[length:0_3px] hover:bg-[length:100%_3px] font-bold shadow-md shadow-secondary hover:scale-[1.02] active:scale-[0.98] duration-300"
+              /* className="rounded-md px-6 py-2 ring-1 ring-inset ring-secondary/10 bg-zinc-800/50 bg-gradient-to-r from-zinc-500 to-zinc-500 hover:from-secondary hover:to-secondary bg-bottom bg-no-repeat bg-[length:0_3px] hover:bg-[length:100%_3px] font-bold shadow-md shadow-secondary active:scale-95 duration-300" */
+              /* className="rounded-md px-6 py-2 ring-1 ring-inset ring-secondary/10 hover:scale-105 bg-zinc-800/50 bg-gradient-to-r from-zinc-500 to-zinc-500 hover:from-primary hover:to-secondary bg-bottom bg-no-repeat bg-[length:100%_3px] font-bold shadow-md shadow-secondary active:scale-100 duration-300" */
+              >
               View my CV
             </Link>
             <Link
               href='https://github.com/AL-NFLY'
               target="_blank"
-              className="rounded-sm px-4 py-1 border dark:border-gray-500 hover:scale-105 font-bold shadow-lg hover:shadow-primary active:scale-100 duration-300"
+              className="rounded-md px-6 py-2 ring-1 ring-inset ring-primary/10 font-bold bg-zinc-800/50 bg-gradient-to-r from-zinc-500 to-zinc-500 hover:from-primary hover:to-primary bg-bottom bg-no-repeat bg-[length:0_3px] hover:bg-[length:100%_3px] shadow-md shadow-primary hover:scale-[1.02] active:scale-[0.98] duration-300"
+              // className="rounded-md px-6 py-2 ring-1 ring-inset ring-primary/10 font-bold bg-zinc-800/50 bg-gradient-to-r from-zinc-500 to-zinc-500 hover:from-primary hover:to-primary bg-bottom bg-no-repeat bg-[length:0_3px] hover:bg-[length:100%_3px] shadow-md shadow-primary active:scale-95 duration-300"
+              // className="rounded-md px-6 py-2 ring-1 ring-inset ring-primary/10 hover:scale-105 font-bold bg-zinc-800/50 bg-gradient-to-r from-zinc-500 to-zinc-500 hover:from-primary hover:to-secondary bg-bottom bg-no-repeat bg-[length:100%_3px] shadow-md shadow-primary active:scale-100 duration-300"
             >
               Visit my Github
             </Link>
-            
 
             {/* <StarCanvas /> */}
           </div>
