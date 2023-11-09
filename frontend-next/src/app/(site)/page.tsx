01cfc0efbb0b0c938526ff90d2c44a037c42b6c9
AL-NFLY <almernfly@gmail.com>
2023-11-09 15:28:18 +0700

    refactor: Move hero code to Hero.tsx

--- frontend-next/src/app/(site)/page.tsx
+++ frontend-next/src/app/(site)/page.tsx
@@ -5,62 +5,16 @@ import Experience from "@/components/Experience";
 import ProjectCards from "@/components/ProjectCards";
 import { IoCaretDown } from "react-icons/io5";
 import CurratedProjectCards from "@/components/CurratedProjectCards";
+import Hero from "@/components/Hero";
 // import { PortableText } from "@portabletext/react";
 
 
 export default async function Home() {
   return (
     <main className="px-8 md:px-48 md:-mt-20 -mt-28">
-      <section className="flex flex-col items-center justify-center min-h-screen ">
-        <article>
-          <h2 className="text-3xl md:text-4xl 2xl:text-5xl font-bold">Hi,</h2>
-          <h1 className="text-4xl md:text-5xl 2xl:text-6xl font-bold">
-            I&apos;m <span className="bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">Almer</span>!
-          </h1>
-          <p className="mt-4 text-dark dark:text-zinc-300 tracking-wide">A Software Engineer & Bachelor of Engineering in Computer Eng. ITS</p>
-
-          <div className="flex mt-6 gap-x-6">
-          <Link
-              href='https://drive.google.com/file/d/1SVcxmopbjgWPImLW-GdryXJAZv1jGiOy/view?usp=sharing'
-              target="_blank"
-              className="rounded-md px-6 py-2 ring-1 ring-inset ring-secondary/10 bg-zinc-50/50 dark:bg-zinc-800/50 bg-gradient-to-r hover:from-secondary hover:to-secondary bg-bottom bg-no-repeat bg-[length:0_3px] hover:bg-[length:100%_3px] font-bold shadow-md shadow-secondary hover:scale-[1.02] active:scale-[0.98] duration-300"
-              /* className="rounded-md px-6 py-2 ring-1 ring-inset ring-secondary/10 bg-zinc-800/50 bg-gradient-to-r from-zinc-500 to-zinc-500 hover:from-secondary hover:to-secondary bg-bottom bg-no-repeat bg-[length:0_3px] hover:bg-[length:100%_3px] font-bold shadow-md shadow-secondary active:scale-95 duration-300" */
-              /* className="rounded-md px-6 py-2 ring-1 ring-inset ring-secondary/10 hover:scale-105 bg-zinc-800/50 bg-gradient-to-r from-zinc-500 to-zinc-500 hover:from-primary hover:to-secondary bg-bottom bg-no-repeat bg-[length:100%_3px] font-bold shadow-md shadow-secondary active:scale-100 duration-300" */
-              >
-              View my CV
-            </Link>
-            <Link
-              href='https://github.com/AL-NFLY'
-              target="_blank"
-              className="rounded-md px-6 py-2 ring-1 ring-inset ring-primary/10 font-bold bg-zinc-50/50 dark:bg-zinc-800/50 bg-gradient-to-r hover:from-primary hover:to-primary bg-bottom bg-no-repeat bg-[length:0_3px] hover:bg-[length:100%_3px] shadow-md shadow-primary hover:scale-[1.02] active:scale-[0.98] duration-300"
-              // className="rounded-md px-6 py-2 ring-1 ring-inset ring-primary/10 font-bold bg-zinc-800/50 bg-gradient-to-r from-zinc-500 to-zinc-500 hover:from-primary hover:to-primary bg-bottom bg-no-repeat bg-[length:0_3px] hover:bg-[length:100%_3px] shadow-md shadow-primary active:scale-95 duration-300"
-              // className="rounded-md px-6 py-2 ring-1 ring-inset ring-primary/10 hover:scale-105 font-bold bg-zinc-800/50 bg-gradient-to-r from-zinc-500 to-zinc-500 hover:from-primary hover:to-secondary bg-bottom bg-no-repeat bg-[length:100%_3px] shadow-md shadow-primary active:scale-100 duration-300"
-            >
-              Visit my Github
-            </Link>
-
-            {/* <StarCanvas /> */}
-          </div>
-        </article>
-        <Link className="absolute bottom-8 md:bottom-12 rounded-full transition-colors duration-300" href='#experience'>
-          <IoCaretDown className='rounded-full h-8 w-8 animate-pulse active:animate-none md:h-10 md:w-10 hover:bg-gradient-to-r hover:from-primary hover:to-secondary active:scale-95 '/>
-        </Link>
-      </section>
-
+      <Hero />
       <Experience />
       <CurratedProjectCards />
-
-      {/* <section className="mt-6 mb-24 xl:mb-48">
-        <div className="flex">
-          <Link
-            href={'/projects'}
-            className="px-4  py-2 font-bold text-sm text-zinc-600 dark:text-zinc-300 border rounded-sm border-zinc-300 dark:border-zinc-500 shadow-sm hover:shadow-primary hover: active:scale-95 duration-300"
-            >
-            View More Projects
-          </Link>
-        </div>
-      </section> */}
-
       <section className="mt-6 mb-24 xl:mb-48">
         <div className="flex">
           <Link
---
+++ frontend-next/src/components/Hero.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link"
+import { IoCaretDown } from "react-icons/io5"
+
+const Hero = () => {
+  return (
+    <section className="flex flex-col items-center justify-center min-h-screen ">
+        <article>
+          <h2 className="text-3xl md:text-4xl 2xl:text-5xl font-bold">Hi,</h2>
+          <h1 className="text-4xl md:text-5xl 2xl:text-6xl font-bold">
+            I&apos;m <span className="bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">Almer</span>!
+          </h1>
+          <p className="mt-4 text-dark dark:text-zinc-300 tracking-wide">A Software Engineer & Bachelor of Engineering in Computer Eng. ITS</p>
+
+          <div className="flex mt-6 gap-x-6">
+          <Link
+              href='https://drive.google.com/file/d/1SVcxmopbjgWPImLW-GdryXJAZv1jGiOy/view?usp=sharing'
+              target="_blank"
+              className="rounded-md px-6 py-2 ring-1 ring-inset ring-secondary/10 bg-zinc-50/50 dark:bg-zinc-800/50 bg-gradient-to-r hover:from-secondary hover:to-secondary bg-bottom bg-no-repeat bg-[length:0_3px] hover:bg-[length:100%_3px] font-bold shadow-md shadow-secondary hover:scale-[1.02] active:scale-[0.98] duration-300"
+              /* className="rounded-md px-6 py-2 ring-1 ring-inset ring-secondary/10 bg-zinc-800/50 bg-gradient-to-r from-zinc-500 to-zinc-500 hover:from-secondary hover:to-secondary bg-bottom bg-no-repeat bg-[length:0_3px] hover:bg-[length:100%_3px] font-bold shadow-md shadow-secondary active:scale-95 duration-300" */
+              /* className="rounded-md px-6 py-2 ring-1 ring-inset ring-secondary/10 hover:scale-105 bg-zinc-800/50 bg-gradient-to-r from-zinc-500 to-zinc-500 hover:from-primary hover:to-secondary bg-bottom bg-no-repeat bg-[length:100%_3px] font-bold shadow-md shadow-secondary active:scale-100 duration-300" */
+              >
+              View my CV
+            </Link>
+            <Link
+              href='https://github.com/AL-NFLY'
+              target="_blank"
+              className="rounded-md px-6 py-2 ring-1 ring-inset ring-primary/10 font-bold bg-zinc-50/50 dark:bg-zinc-800/50 bg-gradient-to-r hover:from-primary hover:to-primary bg-bottom bg-no-repeat bg-[length:0_3px] hover:bg-[length:100%_3px] shadow-md shadow-primary hover:scale-[1.02] active:scale-[0.98] duration-300"
+              // className="rounded-md px-6 py-2 ring-1 ring-inset ring-primary/10 font-bold bg-zinc-800/50 bg-gradient-to-r from-zinc-500 to-zinc-500 hover:from-primary hover:to-primary bg-bottom bg-no-repeat bg-[length:0_3px] hover:bg-[length:100%_3px] shadow-md shadow-primary active:scale-95 duration-300"
+              // className="rounded-md px-6 py-2 ring-1 ring-inset ring-primary/10 hover:scale-105 font-bold bg-zinc-800/50 bg-gradient-to-r from-zinc-500 to-zinc-500 hover:from-primary hover:to-secondary bg-bottom bg-no-repeat bg-[length:100%_3px] shadow-md shadow-primary active:scale-100 duration-300"
+            >
+              Visit my Github
+            </Link>
+
+          </div>
+        </article>
+        <Link className="absolute bottom-8 md:bottom-12 rounded-full transition-colors duration-300" href='#experience'>
+          <IoCaretDown className='rounded-full h-8 w-8 animate-pulse active:animate-none md:h-10 md:w-10 hover:bg-gradient-to-r hover:from-primary hover:to-secondary active:scale-95 '/>
+        </Link>
+    </section>
+  )
+}
+
+export default Hero
