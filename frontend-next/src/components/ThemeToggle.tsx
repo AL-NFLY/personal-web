fc9f6ca9a15b85ae81ad95bc8385ca63f39b2bdd
AL-NFLY <almernfly@gmail.com>
2023-11-11 19:34:28 +0700

    feat: Add animation for themetoggle button

--- frontend-next/src/components/ThemeToggle.tsx
+++ frontend-next/src/components/ThemeToggle.tsx
@@ -25,7 +25,7 @@ const ThemeToggle = () => {
     <button
       aria-label="Toggle Dark Mode"
       type="button"
-      className="flex justify-center items-center transition-colors rounded-md p-1 bg-zinc-200/50 hover:bg-zinc-200 dark:bg-zinc-800/50 dark:hover:bg-zinc-800"
+      className="flex justify-center items-center rounded-md p-1 bg-zinc-200/50 hover:bg-zinc-200 dark:bg-zinc-800/50 dark:hover:bg-zinc-800 bg-gradient-to-r hover:from-primary hover:to-secondary bg-bottom bg-no-repeat bg-[length:0_2px] hover:bg-[length:100%_2px] active:bg-[length:100%_100%] duration-150"
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
     >
       {theme === 'light' ? (
