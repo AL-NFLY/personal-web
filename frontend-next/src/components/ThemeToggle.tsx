737e6928be82dc13ae7d0aef155eb9e6ab31eeac
AL-NFLY <almernfly@gmail.com>
2023-11-12 20:50:42 +0700

    refactor: Refactor onClick

--- frontend-next/src/components/ThemeToggle.tsx
+++ frontend-next/src/components/ThemeToggle.tsx
@@ -21,12 +21,13 @@ const ThemeToggle = () => {
   //   </button>
   // )
   const { theme, setTheme } = useTheme()
+  const clickToggle = () => setTheme(theme === 'dark' ? 'light' : 'dark')
   return (
     <button
       aria-label="Toggle Dark Mode"
       type="button"
       className="flex justify-center items-center rounded-md p-1 bg-zinc-200/50 hover:bg-zinc-200 dark:bg-zinc-800/50 dark:hover:bg-zinc-800 bg-gradient-to-r hover:from-primary hover:to-secondary bg-bottom bg-no-repeat bg-[length:0_2px] hover:bg-[length:100%_2px] active:bg-[length:100%_100%] duration-150"
-      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      onClick={clickToggle}
     >
       {theme === 'light' ? (
         <IoMoon className='h-5 w-5' />
