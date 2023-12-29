a918b8306efabd51336a15fd99fabec3f7cfa3d0
AL-NFLY <almernfly@gmail.com>
2023-12-29 13:19:43 +0700

    feat: Update style of experience description list

--- frontend-next/src/components/Experience.tsx
+++ frontend-next/src/components/Experience.tsx
@@ -98,7 +98,7 @@ const Experience = () => {
             >
               <span className="font-bold text-xl bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">{item.title}</span>
               <p className="text-zinc-400 !mt-0 !text-sm">{item.location}</p>
-              <ul className="list-disc iniside list-inside mt-3 space-y-1 text-sm text-zinc-300 tracking-wide">
+              <ul className="list-disc mt-3 pl-4 space-y-1.5 text-sm text-zinc-300 tracking-wide">
                 <li>{item.descOne}</li>
                 <li>{item.descTwo}</li>
                 <li>{item.descThree}</li>
